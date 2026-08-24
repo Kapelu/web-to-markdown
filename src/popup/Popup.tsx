1f0b2619d716c1a856ca35c59c2c9530e069ba05
kapelu <ddaniel_calderon@hotmail.com>
2026-08-23 21:28:55 -0300

    ✔ Update 23-08-2026

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -42,11 +42,11 @@ export function Popup() {
       const { title, url, content } = response.data
 
       const markdown = [
-      /*  '---',
+        '---',
         `title: "${title.replace(/"/g, '\\"')}"`,
         `url: ${url}`,
         `date: ${new Date().toISOString().slice(0, 10)}`,
-        '---', */
+        '---',
         '',
         content,
       ].join('\n')
@@ -54,7 +54,7 @@ export function Popup() {
       const downloadResponse = (await chrome.runtime.sendMessage({
         type: 'DOWNLOAD_MARKDOWN',
         payload: {
-          /* content: markdown, */
+          content: markdown,
           filename: createMarkdownFilename(title),
         },
       })) as DownloadResponse
